d08e4f26956bab00509e93460aa720b1fca2a5c8
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2025-04-21 12:27:17 +0900

    Create filter_published.rb

---
+++ _plugins/filter_published.rb
@@ -0,0 +1,8 @@
+module Jekyll
+  class PublishedFilter < Jekyll::Generator
+    def generate(site)
+      # 公開済みの記事のみを表示する
+      site.posts.docs.select! { |post| post.data["published"] != false }
+    end
+  end
+end

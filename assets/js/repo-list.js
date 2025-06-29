3cee6f53d586349e04bcbe985f10ee890116fe6f
零鮫/れいさめ <78349683+hrmcngs@users.noreply.github.com>
2025-06-29 15:12:11 +0900

    Update repo-list.js

--- assets/js/repo-list.js
+++ assets/js/repo-list.js
@@ -76,8 +76,11 @@ function displayRepos(repos) {
     `;
     repoListElement.appendChild(repoElement);
 
-    repoElement.addEventListener("click", () => handleRepoClick(repo, repoElement.querySelector(".extra-info")));
-  });
+repoElement.addEventListener("click", (e) => {
+  // aタグクリック時は何もしない
+  if (e.target.tagName === "A") return;
+  handleRepoClick(repo, repoElement.querySelector(".extra-info"));
+});
 }
 
 // リポジトリをクリックしたときの処理（そのまま）

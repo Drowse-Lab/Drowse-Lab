d9e4d9eb52944c55067cf832a8e3caf733c52c0c
零鮫/れいさめ <78349683+hrmcngs@users.noreply.github.com>
2025-07-10 15:05:30 +0900

    Update filter_published.rb

--- _plugins/filter_published.rb
+++ _plugins/filter_published.rb
@@ -2,6 +2,7 @@ module Jekyll
   class PublishedFilter < Jekyll::Generator
     def generate(site)
       # 公開済みの記事のみを表示する
+      # true はその日のみ、nullは常にを追加したい
       site.posts.docs.select! { |post| post.data["published"] != false }
     end
   end
